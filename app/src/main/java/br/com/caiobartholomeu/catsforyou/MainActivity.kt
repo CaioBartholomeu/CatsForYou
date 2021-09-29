743321f28a043de67683af59bd3c8df7ef2e5837
Caio Bartholomeu <caio.sadu@gmail.com>
2021-09-29 19:17:52 -0300

    First Commit
    Initial Settings

---
+++ app/src/main/java/br/com/caiobartholomeu/catsforyou/MainActivity.kt
@@ -0,0 +1,38 @@
+package br.com.caiobartholomeu.catsforyou
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.caiobartholomeu.catsforyou.ui.theme.CatsForYouTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CatsForYouTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("World")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CatsForYouTheme {
+        Greeting("World")
+    }
+}
